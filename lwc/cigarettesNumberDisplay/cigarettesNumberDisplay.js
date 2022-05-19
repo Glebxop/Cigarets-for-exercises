48535635fe425249d27e2738563d56a9cba0af68
“Hleb <hleb_kharoshyn@epam.com>
2022-05-19 15:27:58 +0400

    Added Message Channel

--- lwc/cigarettesNumberDisplay/cigarettesNumberDisplay.js
+++ lwc/cigarettesNumberDisplay/cigarettesNumberDisplay.js
@@ -1,10 +1,42 @@
-/**
+ /**
  * Created by Hleb_Kharoshyn on 18.05.22.
  */
 
-import {LightningElement} from 'lwc';
-import channelName from '@salesforce/messageChannel/channelReference';
-
+import {LightningElement, wire} from 'lwc';
+import {subscribe,unsubscribe,MessageContext} from 'lightning/messageService';
+import myMessageChannel from '@salesforce/messageChannel/MyMessageChannel__c';
 
 export default class CigarettesNumberDisplay extends LightningElement {
+    subscription = null;
+    outputMessage = 0;
+
+    @wire(MessageContext)
+    messageContext;
+
+    connectedCallback(){
+        this.subscribeToMessageChannel();
+    }
+
+    subscribeToMessageChannel() {
+        if (!this.subscription) {
+            this.subscription = subscribe(
+                this.messageContext,
+                myMessageChannel,
+                (message) => this.handleMessageChannel(message)
+            );
+        }
+    }
+
+    disconnectedCallback() {
+        this.unsubscribeToMessageChannel();
+    }
+
+    unsubscribeToMessageChannel() {
+        unsubscribe(this.subscription);
+        this.subscription = null;
+    }
+
+    handleMessageChannel(message){
+        this.outputMessage = this.outputMessage + parseInt(message.message);
+    }
 }
--- lwc/inputNumberModal/inputNumberModal.js
+++ lwc/inputNumberModal/inputNumberModal.js
@@ -2,9 +2,15 @@
  * Created by Hleb_Kharoshyn on 18.05.22.
  */
 
-import {LightningElement, api} from 'lwc';
+import {LightningElement, api, wire} from 'lwc';
+import {publish, MessageContext} from 'lightning/messageService';
+import myMessageChannel from '@salesforce/messageChannel/MyMessageChannel__c';
+
 
 export default class InputNumberModal extends LightningElement {
+
+    @wire(MessageContext)
+    messageContext;
 
     isShowModal = false;
     count;
@@ -24,6 +30,10 @@ export default class InputNumberModal extends LightningElement {
 
     submitInput(){
         console.log(this.count);
+        const payload = { message : this.count };
+        publish(this.messageContext, myMessageChannel, payload);
+        this.count = null;
+        this.closeModal();
     }
 
 }
